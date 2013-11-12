12719a7bbc5287cbe5426979e61fa1c8fae513a0
Oswaldo Bayona <obayona@espol.edu.ec>
2013-11-11 22:42:19 -0500

    Implementacion clase celda

---
+++ Buscaminas/src/com/example/buscaminas/Celda.java
@@ -0,0 +1,61 @@
+package com.example.buscaminas;
+
+import java.util.Observable;
+import java.util.Observer;
+
+
+import android.content.Context;
+import android.widget.Button;
+
+public class Celda extends Button implements Observer{
+	boolean vacio;
+	boolean esMina;
+	EstadoCelda estado;
+	int n_minas_cercanas;
+	int pos_x,pos_y;
+	private CeldaObservable observable;
+	
+	public Celda(Context context,int i,int j) {
+		super(context);
+		vacio=false;
+		esMina=false;
+		estado=EstadoCelda.CUBIERTA;
+		n_minas_cercanas=0;
+		pos_x=i;
+		pos_y=j;
+	}
+	
+	public void descubrir(){
+		
+		
+		estado=EstadoCelda.DESCUBIERTA;
+	
+	
+		
+		
+		
+		
+	
+	}
+	
+	public CeldaObservable getObservable() {
+		return observable;
+	}
+
+	class CeldaObservable extends Observable{
+		void cambioCelda(){
+			setChanged();
+		}
+	}
+	
+	
+
+	@Override
+	public void update(Observable arg0, Object arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+
+}
---
+++ Buscaminas/src/com/example/buscaminas/EstadoCelda.java
@@ -0,0 +1,5 @@
+package com.example.buscaminas;
+
+public enum EstadoCelda {
+	CUBIERTA,DESCUBIERTA
+}
