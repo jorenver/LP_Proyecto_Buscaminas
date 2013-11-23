2ad240057e399d6f2b0cb6508bf69ee04af97b12
obayona <obayona@espol.edu.ec>
2013-11-23 15:33:11 -0500

    Creacion del layout ingresotop que se muestra al entrar el jugador al top y pide su nombre

--- Buscaminas/gen/com/example/buscaminas/R.java
+++ Buscaminas/gen/com/example/buscaminas/R.java
@@ -49,10 +49,14 @@ public final class R {
         public static final int PartidaNueva=0x7f080007;
         public static final int TableLayout1=0x7f080000;
         public static final int Top=0x7f080009;
-        public static final int action_settings=0x7f08000a;
+        public static final int action_settings=0x7f08000e;
+        public static final int button1=0x7f08000d;
+        public static final int editText1=0x7f08000c;
         public static final int tableRow1=0x7f080001;
         public static final int tableRow2=0x7f080003;
         public static final int tableRow3=0x7f080005;
+        public static final int textView1=0x7f08000a;
+        public static final int textView2=0x7f08000b;
     }
     public static final class layout {
         public static final int activity_acerca_de=0x7f030000;
@@ -63,6 +67,7 @@ public final class R {
         public static final int boton_acerca_de=0x7f030005;
         public static final int boton_partida_nueva=0x7f030006;
         public static final int boton_top_jugadores=0x7f030007;
+        public static final int ingresotop=0x7f030008;
     }
     public static final class menu {
         public static final int acerca_de=0x7f070000;
@@ -71,16 +76,20 @@ public final class R {
         public static final int top_jugadores=0x7f070003;
     }
     public static final class string {
+        public static final int Aceptar=0x7f05000c;
         public static final int Desarrolladores=0x7f050005;
         public static final int DificultadDelJuego=0x7f050009;
         public static final int TOP=0x7f050007;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int busca_Minas=0x7f050002;
+        public static final int camponombretop=0x7f05000d;
         public static final int hello_world=0x7f050004;
+        public static final int nombre=0x7f05000b;
         public static final int title_activity_acerca_de=0x7f050006;
         public static final int title_activity_inicio=0x7f050003;
         public static final int title_activity_top_jugadores=0x7f050008;
+        public static final int titulotop=0x7f05000a;
     }
     public static final class style {
         /** 
